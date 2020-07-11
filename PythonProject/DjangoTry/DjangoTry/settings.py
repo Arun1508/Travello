a9c52622b6f1dfea0143a1d599cbaf7c32dde2bc
Arun1508 <49265519+Arun1508@users.noreply.github.com>
2020-07-11 12:49:14 +0530

    Update settings.py

--- PythonProject/DjangoTry/DjangoTry/settings.py
+++ PythonProject/DjangoTry/DjangoTry/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '2@uii$jet7fb)(xz87@)**v4%lv#8h4pe+wgnco$b9cw-mivd='
-os.environ['SECRET_KEY']
+os.environ['SECRET_KEY'] = '2@uii$jet7fb)(xz87@)**v4%lv#8h4pe+wgnco$b9cw-mivd='
 SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
